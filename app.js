cb66138f78f199fef47d18ade8e6d8f1ad6b8c2e
Alexey Kuptsov <60064512+jerubrin@users.noreply.github.com>
2022-01-24 22:49:15 +0600

    Chage design

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const io = require("socket.io")(server);
 io.on('connection', (socket) => {
 	console.log('New user connected')
 
-	socket.username = "Anonymous"
+	socket.username = "Гость"
 
     socket.on('change_username', (data) => {
         socket.username = data.username
--- public/chat.js
+++ public/chat.js
@@ -1,12 +1,18 @@
+var readyToSent = true;
+var send_message = $("#send_message");
+
 $(function() {
   var socket = io.connect("http://localhost:3000");
 
   var message = $("#message");
   var username = $("#username");
-  var send_message = $("#send_message");
   var send_username = $("#send_username");
   var chatroom = $("#chatroom");
   var feedback = $("#feedback");
+  var change_username = $("#change_username");
+  var input_zone = $("#input_zone");
+  var isSeted = false;
+  var msgnum = 0;
 
   send_message.click(() => {
     socket.emit("new_message", {
@@ -46,18 +52,47 @@ $(function() {
     feedback.html("");
     message.val("");
     chatroom.append(
-      "<div class='alert alert-" +
-        data.className +
-        "'<b>" +
+      "<div class='" + 
+        "alert alert-" + data.className + "' id='num" + msgnum.toString() +
+        "'>" + 
+        "<b>" +
         data.username +
         "</b>: " +
         data.message +
         "</div>"
     );
+    autoHide("#num" + msgnum.toString())
+    msgnum++
   });
+    
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        if (isSeted) {
+            if((message.val() != "") && readyToSent) {
+                socket.emit("new_message", {
+                    message: message.val(),
+                    className: alertClass
+                });
+                waitForNextMsg();
+            }
+        } else {
+            if(username.val() != "") {
+                socket.emit("change_username", { username: username.val() });
+                change_username.css("display", "none");
+                input_zone.css("display", "flex");
+                isSeted = true;
+            }
+        }
+    }
+});
 
   send_username.click(() => {
-    socket.emit("change_username", { username: username.val() });
+      if((username.val() != "") && readyToSent) {
+          socket.emit("change_username", { username: username.val() });
+          change_username.css("display", "none");
+          input_zone.css("display", "flex");
+          isSeted = true;
+      }
   });
 
   message.bind("keypress", () => {
@@ -70,3 +105,23 @@ $(function() {
     );
   });
 });
+
+function autoHide(className) {
+    var fClass = $(document).find(className)
+    setTimeout(hideClass, 30000, fClass);
+}
+
+function hideClass(fClass) {
+    fClass.css("display", "none")
+}
+
+function waitForNextMsg() {
+    readyToSent = false;
+    send_message.css("background", "#b6b7d18a")
+    setTimeout(sendNext, 5000);
+}
+
+function sendNext() {
+    send_message.css("background", "#0e128199")
+    readyToSent = true
+}
